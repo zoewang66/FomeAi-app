898b9a6c18f4087e08f57dbfe1ca07c82985b624
JesseGuSH <gcl522@hotmail.com>
2024-04-27 14:55:53 +1000

    ChallengeBox

---
+++ src/components/IndividualExercise.js
@@ -0,0 +1,7 @@
+import { TouchableOpacity } from "react-native";
+
+import { View, Text, Image, StyleSheet } from "react-native";
+
+export default function IndividualExercise({ name }) {
+  return <TouchableOpacity></TouchableOpacity>;
+}
