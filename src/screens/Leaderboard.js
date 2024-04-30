404aefec8527fb548737b9a3ce6b83c47809a0c5
TheGigaNerd <152786084+TheGigaNerd@users.noreply.github.com>
2024-04-30 23:19:47 +1000

    Number 1 now displayed in the middle

--- src/screens/Leaderboard.js
+++ src/screens/Leaderboard.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { View, Text, FlatList, StyleSheet, Image } from "react-native";
+import NavbarTop from "../components/Navbar-top";
 
 export function LeaderBoard({ leaderboardProps }) {
   const sortedLeaders = leaderboardProps
     .slice()
     .sort((a, b) => a.position - b.position);
 
-  const topLeaders = sortedLeaders.slice(0, 3);
+  const number1 = sortedLeaders.slice(0, 1);
+  const number2 = sortedLeaders.slice(1, 2);
+  const number3 = sortedLeaders.slice(2, 3);
   const remainingLeaders = sortedLeaders.slice(3);
 
   const renderLeader = ({ item, index }) => (
@@ -20,14 +23,40 @@ export function LeaderBoard({ leaderboardProps }) {
 
   return (
     <View>
+      <NavbarTop />
       <Text style={styles.text}>Leaderboard</Text>
       <View style={styles.topLeadersContainer}>
-        {topLeaders.map((leader) => (
-          <View key={leader.name} style={styles.topLeader}>
+        {number2.map((leader) => (
+          <View
+            key={leader.name}
+            style={[styles.topLeader, styles.silverBackground]}
+          >
+            <Image source={leader.image} style={styles.imageLeader} />
             <Text style={styles.position}>{leader.position}</Text>
-            <Text style={styles.name}>{leader.name}</Text>
-            <Text style={styles.score}>{leader.score}</Text>
-            <Image source={leader.image} style={styles.image} />
+            <Text style={styles.leaderName}>{leader.name}</Text>
+            <Text style={styles.leaderScore}>{leader.score}</Text>
+          </View>
+        ))}
+        {number1.map((leader) => (
+          <View
+            key={leader.name}
+            style={[styles.topLeader, styles.goldBackground]}
+          >
+            <Image source={leader.image} style={styles.imageLeader} />
+            <Text style={styles.position}>{leader.position}</Text>
+            <Text style={styles.leaderName}>{leader.name}</Text>
+            <Text style={styles.leaderScore}>{leader.score}</Text>
+          </View>
+        ))}
+        {number3.map((leader) => (
+          <View
+            key={leader.name}
+            style={[styles.topLeader, styles.bronzeBackground]}
+          >
+            <Image source={leader.image} style={styles.imageLeader} />
+            <Text style={styles.position}>{leader.position}</Text>
+            <Text style={styles.leaderName}>{leader.name}</Text>
+            <Text style={styles.leaderScore}>{leader.score}</Text>
           </View>
         ))}
       </View>
@@ -52,6 +81,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     margin: 30,
+    marginLeft: 125,
   },
   listContainer: {
     flexGrow: 1,
@@ -71,12 +101,23 @@ const styles = StyleSheet.create({
   name: {
     flex: 1,
     fontSize: 16,
+    marginLeft: 10,
+  },
+  leaderName: {
+    flex: 1,
+    fontSize: 18,
   },
   score: {
     fontSize: 16,
     fontWeight: "bold",
     marginRight: 10,
     backgroundColor: "#DBFFA1",
+  },
+  leaderScore: {
+    fontSize: 18,
+    fontWeight: "bolder",
+    margin: "auto",
+    backgroundColor: "gold",
   },
   position: {
     fontSize: 16,
@@ -88,22 +129,38 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     marginLeft: 20,
   },
+  imageLeader: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
   topLeadersContainer: {
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     marginBottom: 20,
+    alignItems: "center",
+    backgroundColor: "DBFFA1",
   },
   topLeader: {
     alignItems: "center",
     width: "20%",
     marginHorizontal: "5%",
-    marginRight: 10, // Corrected margin value
+    marginRight: 10,
     borderWidth: 1,
     borderColor: "#ccc",
     padding: 10,
   },
   itemAlternate: {
     backgroundColor: "#F5F4F1",
+  },
+  goldBackground: {
+    backgroundColor: "gold",
+  },
+  silverBackground: {
+    backgroundColor: "silver",
+  },
+  bronzeBackground: {
+    backgroundColor: "bronze",
   },
 });
 
