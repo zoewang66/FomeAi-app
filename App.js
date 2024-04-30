16d2d6daae8a58e9f6e052e44ead4fb67ac9c0dc
TheGigaNerd <152786084+TheGigaNerd@users.noreply.github.com>
2024-04-30 22:01:35 +1000

    Update App.js
    Added leaderboard component and props

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ import CardioAssessmentScreen from "./src/screens/Cardio-Assessment";
 import StrengthAssessmentScreen from "./src/screens/Strength-Assessment";
 =======
 import Exercise from "./src/screens/Exercise";
+import Leaderboard from "./src/screens/Leaderboard";
 >>>>>>> Stashed changes
 
 export default function App() {
@@ -31,6 +32,7 @@ export default function App() {
     // <SafeAreaView style={styles.safeArea}>
     //   <Congratulations />
       /* //   <NavigationContainer>
+      <Leaderbord />
 =======
     <SafeAreaView style={styles.safeArea}>
       <Exercise />
@@ -50,6 +52,69 @@ export default function App() {
     // </SafeAreaView>
   );
 };
+const leaderboardProps = [
+  {
+    name: "Jesse",
+    score: 97,
+    position: 1,
+    image: require("./imgs/number1.png"),
+  },
+  {
+    name: "Zoe",
+    score: 85,
+    position: 2,
+    image: require("./imgs/number2.png"),
+  },
+  {
+    name: "Dylan",
+    score: 80,
+    position: 3,
+    image: require("./imgs/number3.png"),
+  },
+  {
+    name: "Bob",
+    score: 78,
+    position: 4,
+    image: require("./imgs/number4.png"),
+  },
+  {
+    name: "Sasha",
+    score: 77,
+    position: 5,
+    image: require("./imgs/number5.png"),
+  },
+  {
+    name: "Leo",
+    score: 72,
+    position: 6,
+    image: require("./imgs/number6.png"),
+  },
+  {
+    name: "Jason",
+    score: 69,
+    position: 7,
+    image: require("./imgs/number7.png"),
+  },
+  {
+    name: "Loretta",
+    score: 65,
+    position: 8,
+    image: require("./imgs/number8.png"),
+  },
+  {
+    name: "Chris",
+    score: 64,
+    position: 9,
+    image: require("./imgs/number9.png"),
+  },
+  {
+    name: "Tiffany",
+    score: 62,
+    position: 10,
+    image: require("./imgs/number10.png"),
+  },
+];
+
 
 const styles = StyleSheet.create({
   safeArea: {
